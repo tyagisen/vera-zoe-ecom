d21c6fdafcb10d940a3167955d2f9ae56725b7d0
tyagisen <tyagisen@gmail.com>
2026-04-09 17:02:28 +0100

    Add order models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from django.conf import settings
 from apps.common.models import BaseModel
 from apps.products.models import ProductVariant
+from .choices import StatusChoices, PaymentStatusChoices
 
 
 User = settings.AUTH_USER_MODEL
@@ -25,4 +26,39 @@ class Address(BaseModel):
     def __str__(self):
         return f"{self.full_name} - {self.address_line_1}, {self.city}"
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    address = models.ForeignKey(Address, on_delete=models.PROTECT, related_name="orders")
+    status = models.CharField(max_length=20, choices = StatusChoices.choices, default=StatusChoices.PENDING)
+    payment_status = models.CharField(max_length=20, choices = PaymentStatusChoices.choices, default=PaymentStatusChoices.UNPAID)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
+    shipping_cost = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
+    tax = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
+
+
+    class Meta:
+        ordering= ["-created_at"]
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    variant = models.ForeignKey(ProductVariant, on_delete=models.PROTECT, related_name="order_items")
+    product_name = models.CharField(max_length=255)
+    sku = models.CharField(max_length=80)
+    size = models.CharField(max_length=20)
+    color = models.CharField(max_length=50)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=20, decimal_places=2)
+    total_price = models.DecimalField(max_digits=20, decimal_places=2)
+
+
+    class Meta:
+        ordering=["id"]
+
     
+    def __str__(self):
+        return f"{self.product_name}  * {self.quantity}"
