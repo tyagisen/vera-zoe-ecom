ce14dee1465df229c8d2f8e4c3c6b65b652c711d
tyagisen <tyagisen@gmail.com>
2026-05-05 18:25:23 +0100

    Add urls for order

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
-
-urlpatterns = []
+from apps.orders.views import AddressListCreateAPIView, OrderCreateAPIView, OrderDetailAPIView, OrderListAPIView
+urlpatterns = [
+    path("address/", AddressListCreateAPIView.as_view(), name= "address-list-create"),
+    path("", OrderListAPIView.as_view(), name="order-list"),
+    path("create/", OrderCreateAPIView.as_view(), name="order-create"),
+    path("<int:pk>/", OrderDetailAPIView.as_view(), name="order-detail")
+]
